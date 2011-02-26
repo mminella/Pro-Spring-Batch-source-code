8613604d0ad1dd634b67e6b9d5bd785331d2b61c
Michael Minella <michael@michaelminella.com>
2011-02-26 17:48:33 -0600

    Added Jdbc processing to the examples

---
+++ Chapter 9/item_writers/src/main/java/com/apress/springbatch/chapter9/CustomerItemPreparedStatementSetter.java
@@ -0,0 +1,23 @@
+package com.apress.springbatch.chapter9;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+
+public class CustomerItemPreparedStatementSetter implements
+        ItemPreparedStatementSetter<Customer> {
+
+    public void setValues(Customer customer, PreparedStatement ps)
+            throws SQLException {
+
+        ps.setString(1, customer.getFirstName());
+        ps.setString(2, customer.getMiddleInitial());
+        ps.setString(3, customer.getLastName());
+        ps.setString(4, customer.getAddress());
+        ps.setString(5, customer.getCity());
+        ps.setString(6, customer.getState());
+        ps.setString(7, customer.getZip());
+
+    }
+}
