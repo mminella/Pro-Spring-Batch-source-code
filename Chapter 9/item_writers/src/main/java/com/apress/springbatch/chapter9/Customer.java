151554c58cbbfbb3e39e28adaadced0350d23394
Michael Minella <michael@michaelminella.com>
2011-02-27 21:04:46 -0600

    JMS example

--- Chapter 9/item_writers/src/main/java/com/apress/springbatch/chapter9/Customer.java
+++ Chapter 9/item_writers/src/main/java/com/apress/springbatch/chapter9/Customer.java
@@ -1,5 +1,7 @@
 package com.apress.springbatch.chapter9;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,7 +10,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="customer")
-public class Customer {
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
