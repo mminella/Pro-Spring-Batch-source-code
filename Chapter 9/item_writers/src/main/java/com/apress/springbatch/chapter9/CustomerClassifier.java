81a70e4c41edf96cd69cc63cd3fa4c8e2bc1aa00
Michael Minella <michael@michaelminella.com>
2011-03-05 12:35:21 -0600

    completed basic examples

---
+++ Chapter 9/item_writers/src/main/java/com/apress/springbatch/chapter9/CustomerClassifier.java
@@ -0,0 +1,28 @@
+package com.apress.springbatch.chapter9;
+
+import org.springframework.batch.classify.Classifier;
+import org.springframework.batch.item.ItemWriter;
+
+public class CustomerClassifier implements Classifier<Customer, ItemWriter<Customer>> {
+
+    private ItemWriter<Customer> fileItemWriter;
+    private ItemWriter<Customer> jdbcItemWriter;
+
+    @Override
+    public ItemWriter<Customer> classify(Customer customer) {
+        System.out.println("Does " + customer.getState() + " match ^[A-M] ? " + customer.getState().matches("^[A-M].*"));
+        if(customer.getState().matches("^[A-M].*")) {
+            return fileItemWriter;
+        } else {
+            return jdbcItemWriter;
+        }
+    }
+
+    public void setFileItemWriter(ItemWriter<Customer> fileItemWriter) {
+        this.fileItemWriter = fileItemWriter;
+    }
+
+    public void setJdbcItemWriter(ItemWriter<Customer> jdbcItemWriter) {
+        this.jdbcItemWriter = jdbcItemWriter;
+    }
+}
