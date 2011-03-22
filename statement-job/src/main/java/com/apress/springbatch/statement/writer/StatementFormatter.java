c597656314e1ee07129a3a26e17fa0073d6f16e5
Michael Minella <michael@michaelminella.com>
2011-03-22 01:00:00 -0500

    updated

--- statement-job/src/main/java/com/apress/springbatch/statement/writer/StatementFormatter.java
+++ statement-job/src/main/java/com/apress/springbatch/statement/writer/StatementFormatter.java
@@ -11,11 +11,18 @@ import com.apress.springbatch.statement.domain.Transaction;
 public class StatementFormatter implements LineAggregator<Statement> {
 
     private static final String ADDRESS_FORMAT = "%s %s\n%s\n%s, %s %s\n\n";
-    private static final String SUMMARY_HEADER_FORMAT = "Account Number   %s\n\nYour Account Summary\n\n";
-    private static final String SUMMARY_FORMAT = "Market Value of Current Securities                             %s\nCurrent Cash Balance                                                %s\nTotal Account Value                                            %s\n\n";
-    private static final String CASH_DETAIL_FORMAT = "Account Detail\n\nCash                                                      %s\n\nSecurities\n\n";
-    private static final String SECURITY_HOLDING_FORMAT = "     %s                       %s                           %s\n";
-    private static NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance();
+    private static final String SUMMARY_HEADER_FORMAT = "Account Number   %s\n"
+            + "\nYour Account Summary\n\n";
+    private static final String SUMMARY_FORMAT = "Market Value of Current Securities"
+            + "                             %s\nCurrent Cash Balance                   "
+            + "                             %s\nTotal Account Value                     "
+            + "                       %s\n\n";
+    private static final String CASH_DETAIL_FORMAT = "Account Detail\n\nCash        "
+            + "                                              %s\n\nSecurities\n\n";
+    private static final String SECURITY_HOLDING_FORMAT = "     %s                  "
+            + "     %s                           %s\n";
+    private static NumberFormat moneyFormatter = NumberFormat
+            .getCurrencyInstance();
 
     public String aggregate(Statement statement) {
         StringBuilder output = new StringBuilder();
@@ -28,21 +35,34 @@ public class StatementFormatter implements LineAggregator<Statement> {
     }
 
     private void formatDetails(Statement statement, StringBuilder output) {
-        output.append(String.format(CASH_DETAIL_FORMAT, new Object [] {statement.getCustomer().getAccount().getCashBalance()}));
+        output.append(String.format(CASH_DETAIL_FORMAT,
+                new Object[] { statement.getCustomer().getAccount()
+                        .getCashBalance() }));
         for (Transaction transaction : statement.getStocks()) {
-            output.append(String.format(SECURITY_HOLDING_FORMAT, new Object [] {transaction.getTicker(), transaction.getQuantity(), moneyFormatter.format(transaction.getDollarAmount())}));
+            output.append(String.format(SECURITY_HOLDING_FORMAT, new Object[] {
+                    transaction.getTicker(), transaction.getQuantity(),
+                    moneyFormatter.format(transaction.getDollarAmount()) }));
         }
     }
 
     private void formatSummary(Statement statement, StringBuilder output) {
-        output.append(String.format(SUMMARY_HEADER_FORMAT, new Object[] { statement.getCustomer()
-                .getAccount().getAccountNumber() }));
-        output.append(String.format(SUMMARY_FORMAT, new Object [] {moneyFormatter.format(statement.getSecurityTotal()), moneyFormatter.format(statement.getCustomer().getAccount().getCashBalance()), moneyFormatter.format(statement.getCustomer().getAccount().getCashBalance().doubleValue() + statement.getSecurityTotal().doubleValue())}));
+        output.append(String.format(SUMMARY_HEADER_FORMAT,
+                new Object[] { statement.getCustomer().getAccount()
+                        .getAccountNumber() }));
+        output.append(String.format(
+                SUMMARY_FORMAT,
+                new Object[] {
+                        moneyFormatter.format(statement.getSecurityTotal()),
+                        moneyFormatter.format(statement.getCustomer()
+                                .getAccount().getCashBalance()),
+                        moneyFormatter.format(statement.getCustomer()
+                                .getAccount().getCashBalance().doubleValue()
+                                + statement.getSecurityTotal().doubleValue()) }));
     }
 
     private void formatAddress(Statement statement, StringBuilder output) {
         Customer customer = statement.getCustomer();
-        
+
         output.append(String.format(ADDRESS_FORMAT,
                 new Object[] { customer.getFirstName(), customer.getLastName(),
                         customer.getAddress().getAddress1(),
--- statement-job/src/main/java/com/apress/springbatch/statement/writer/StatementHeaderCallback.java
+++ statement-job/src/main/java/com/apress/springbatch/statement/writer/StatementHeaderCallback.java
@@ -8,11 +8,15 @@ import org.springframework.batch.item.file.FlatFileHeaderCallback;
 public class StatementHeaderCallback implements FlatFileHeaderCallback {
 
     public void writeHeader(Writer writer) throws IOException {
-        writer.write("                                                                      Brokerage Account Statement\n");
+        writer.write("                                             " +
+        		"                         Brokerage Account Statement\n");
         writer.write("\n\n");
-        writer.write("Apress Investment Company                                             Customer Service Number\n");
-        writer.write("1060 W. Addison St.                                                   (800) 876-5309\n");
-        writer.write("Chicago, IL 60613                                                     Available 24/7\n");
+        writer.write("Apress Investment Company                     " +
+        		"                        Customer Service Number\n");
+        writer.write("1060 W. Addison St.                            " +
+        		"                       (800) 876-5309\n");
+        writer.write("Chicago, IL 60613                              " +
+        		"                       Available 24/7\n");
         writer.write("\n\n");
     }
 
