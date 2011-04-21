1d01cd7e69a038f0ff7e0ed001add8252373287b
Michael Minella <michael@michaelminella.com>
2011-04-20 23:21:47 -0500

    Added minimal documentation

--- Chapter 2/hello-world/src/main/java/com/apress/springbatch/chapter2/HelloWorld.java
+++ Chapter 2/hello-world/src/main/java/com/apress/springbatch/chapter2/HelloWorld.java
@@ -5,10 +5,18 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet; 
 import org.springframework.batch.repeat.RepeatStatus; 
 
+/**
+* The HelloWorld tasklet is used to write out a simple message to 
+* standard out durring the processing of your job.
+*/
 public class HelloWorld implements Tasklet { 
 
      private static final String HELLO_WORLD = "Hello, world!"; 
 
+	 /**
+	 * {@inheritDoc}
+	 */
+     @Override
      public RepeatStatus execute( StepContribution arg0, ChunkContext arg1 ) throws Exception {
           System.out.println( HELLO_WORLD );
           return RepeatStatus.FINISHED; 
