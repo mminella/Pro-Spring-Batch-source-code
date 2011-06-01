c7f91ef28886f7d5838e11a415a8821fb4de2d28
Michael Minella <michael@michaelminella.com>
2011-05-31 22:40:56 -0500

    Removed unused code

--- statement-job/src/main/java/com/apress/springbatch/statement/dao/TransactionDao.java
+++
@@ -1,7 +0,0 @@
-package com.apress.springbatch.statement.dao;
-
-import com.apress.springbatch.statement.domain.Transaction;
-
-public interface TransactionDao {
-    void saveTransaction(Transaction transaction);
-}
